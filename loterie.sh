231f859d4d75a4b591ba7ba9b44dc522fc9a46be
Jean Zundel <jzu@free.fr>
2017-09-09 10:12:06 +0200

    Cheine de cnx simple en commentaire

--- loterie.sh
+++ loterie.sh
@@ -18,8 +18,8 @@ export BC_LINE_LENGTH=0
 
 #export DEBUG=1
 
-#export GETHEXEC="geth attach $HOME/.local/share/io.parity.ethereum/jsonrpc.ipc --exec"
-export GETHEXEC="geth attach --exec"
+export GETHEXEC="geth attach $HOME/.local/share/io.parity.ethereum/jsonrpc.ipc --exec"
+#export GETHEXEC="geth attach --exec"
 
 
 debug () {
