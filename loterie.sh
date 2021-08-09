afbeb3654826654be66f83c913a70b1c1d731f62
Jean Zundel <jzu@free.fr>
2021-08-09 22:03:21 +0200

    Acces a un noeud Avalanche

--- loterie.sh
+++ loterie.sh
@@ -3,10 +3,11 @@
 # Loterie Asseth - septembre 2017
 # filtertron - jzu@free.fr
 # Compare en valeur absolue le delta entre le hash d'un bloc decide a l'avance
-# et ceux des adresses email des candidats
+# et ceux des adresses email (ou les identifiants) des candidats
 # Le plus petit delta apparait en premier et gagne le lot
 # DEBUG=1 permet d'envoyer les hashs sur stderr pour verification
 # Teste sur une machine ou tourne Parity 1.6.3, changer GETHEXEC au besoin
+# On peut aussi acceder a un noeud Avalanche en specifiant l'URL (port 9650)
 # Exemple :
 # $ ./loterie.sh 4243679 foo@example.com bar@example.com baz@example.com
 
@@ -18,8 +19,9 @@ export BC_LINE_LENGTH=0
 
 #export DEBUG=1
 
-export GETHEXEC="geth attach $HOME/.local/share/io.parity.ethereum/jsonrpc.ipc --exec"
-#export GETHEXEC="geth attach --exec"
+#export GETHEXEC="geth attach $HOME/.local/share/io.parity.ethereum/jsonrpc.ipc --exec"
+export GETHEXEC="geth attach --exec"
+#export GETHEXEC="geth attach http://localhost:9650/ext/bc/C/rpc --exec"
 
 # Affiche les hashes et les deltas
 
